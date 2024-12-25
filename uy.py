e9ec759557000e0533e78532da2a706c767479dc
unknown <karimovf_22@icloud.com>
2024-12-25 18:41:17 +0500

    add 9 uy ishi

---
+++ uy.py
@@ -0,0 +1,51 @@
+import time,os
+from multiprocessing import Process,current_process
+from threading import Thread, current_thread
+from time import sleep
+
+
+def list_work(a:list):
+
+    p_id=os.getpid()
+    print(f"{p_id}----{current_process().name}---{current_thread().name}----Jarayon boshlandi")
+    juft = 1
+    toq = 1
+    s = 0
+    time.sleep(5)
+    for i in a:
+        if i%2 == 0:
+            juft *= i
+        elif i%2 == 1:
+            toq*=i
+
+    print(f"Natija == {(juft-toq)}")
+    print(f"{current_process().name} ---- {current_thread().name} --- jarayon yakunlandi")
+
+
+if  __name__ == "__main__":
+    d = list(range(1, 40))
+    s_time=time.time()
+# 1 single threed  vaqt= 10.002508401870728
+    list_work(d)
+    list_work(d)
+
+# 2 multithreed   vaqt= 5.181133270263672
+#     f1=Thread(target=list_work,args=(d ,))
+#     f2=Thread(target=list_work,args=(d ,))
+#     f1.start()
+#     f2.start()
+#
+#     f1.join()
+#     f2.join()
+
+# 3 multiprogres   vaqt= 5.774171829223633
+#     m1 = Process(target=list_work,args=(d ,))
+#     m2 = Process(target=list_work,args=(d ,))
+#
+#     m1.start()
+#     m2.start()
+#
+#     m1.join()
+#     m2.join()
+    e_time = time.time()
+    print("vaqt=", e_time - s_time)
